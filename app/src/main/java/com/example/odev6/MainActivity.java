e7e9ce4b9177ba90995372c05f81cbc7019e509c
EmirhanAytekin <EmirhanAytekin@users.noreply.github.com>
2022-12-25 15:37:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/odev6/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.odev6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.odev6.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public void kullaniciGirisi(View view){
+        String kullaniciAdi = binding.username.getText().toString();
+        String parola = binding.password.getText().toString();
+
+        Singleton singleton = Singleton.getInstance();
+        singleton.setKullaniciAdi(kullaniciAdi);
+        singleton.setKullaniciParola(parola);
+
+        Intent intent = new Intent(this,AnaSayfa.class);
+        startActivity(intent);
+    }
+}
